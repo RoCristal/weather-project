f2c56f126204cd28f527f1be54ff0e643d69f397
RoCristal <mail@rominacristaldo.com>
2022-03-18 13:07:29 +0100

    Added repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        Coded by Romina Cristaldo
+        <a href="https://github.com/RoCristal/weather-project" target="_blank">
+          Open source on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
