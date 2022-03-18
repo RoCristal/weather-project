b36d1564ebf840f00e5efd1129c2d720d358b3d7
RoCristal <mail@rominacristaldo.com>
2022-03-18 18:56:53 +0100

    Basic styling

--- src/Weather.js
+++ src/Weather.js
@@ -4,19 +4,36 @@ import "./Weather.css";
 export default function Weather() {
   return (
     <div className="weather">
-      <div className="btn btn-primary">Search</div>
-      <h1>New York</h1>
-      <ul>
-        <li>Friday 13:00</li>
-        <li>Fog</li>
-      </ul>
-      <div className="row">
+      <div className="search-bar d-flex rounded">
+        <div className="row">
+          <div className="col-9">
+            <input
+              className=""
+              type="search"
+              placeholder="Enter a city"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <span className="btn btn-outline-secondary btn-sm">Search</span>
+          </div>
+        </div>
+      </div>
+      <div className="weather-summary">
+        <h1>New York</h1>
+        <ul>
+          <li>Friday 13:00</li>
+          <li>Fog</li>
+        </ul>
+      </div>
+      <div className="weather-details row">
         <div className="col-6">
           <img
-            src="https://ssl.gstatic.com/onebox/weather/64/fog.png"
+            src="https://ssl.gstatic.com/onebox/weather/48/rain.png"
             alt="fog"
           />
-          <span className="temp">10°C</span>
+          <span className="temperature">10</span>
+          <span className="unit">°C</span>
         </div>
         <div className="col-6">
           <ul>
@@ -25,7 +42,6 @@ export default function Weather() {
           </ul>
         </div>
       </div>
-      Hello from weather
     </div>
   );
 }
